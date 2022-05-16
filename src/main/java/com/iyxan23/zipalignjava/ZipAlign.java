ce830808c9b59536b62fe1d33cc492655de85cc8
Iyxan23 <nurihsanalghifari@gmail.com>
2022-05-16 20:15:24 +0700

    feat: implement data descriptor
    the resulting zip seems to work fine, passed the zipalign verification, but produces a different hash from the real zipaligned zip

--- src/main/java/com/iyxan23/zipalignjava/ZipAlign.java
+++ src/main/java/com/iyxan23/zipalignjava/ZipAlign.java
@@ -5,6 +5,7 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ZipAlign {
     /**
@@ -24,13 +25,23 @@ public class ZipAlign {
 
         // source: https://en.wikipedia.org/wiki/ZIP_(file_format)#Structure
         // better source: https://users.cs.jmu.edu/buchhofp/forensics/formats/pkzip.html
+        // the real source: https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT
 
-        // starts with local header signature
-        while (inBuffer.getInt() == 0x04034b50) {
+        int header = inBuffer.getInt();
+
+        // starts with local file header signature
+        while (header == 0x04034b50) {
+            System.out.println("at file " + outStream.bytesWritten());
             fileOffsets.add(outStream.bytesWritten());
             outStream.writeInt(0x04034b50);
 
-            passBytes(inBuffer, outStream, 4);
+            passBytes(inBuffer, outStream, 2);
+
+            short generalPurposeFlag = inBuffer.getShort();
+            outStream.writeShort(generalPurposeFlag);
+
+            // data descriptor is used if the 3rd bit is 1
+            boolean hasDataDescriptor = (generalPurposeFlag & 0x8) == 0x8;
 
             short compressionMethod = inBuffer.getShort();
             outStream.writeShort(compressionMethod);
@@ -60,7 +71,11 @@ public class ZipAlign {
                 outStream.writeShort(extraFieldLen);
             }
 
-            passBytes(inBuffer, outStream, fileNameLen);
+            byte[] fileName = new byte[fileNameLen];
+
+            inBuffer.get(fileName);
+            outStream.write(fileName);
+
             passBytes(inBuffer, outStream, extraFieldLen);
 
             if (shouldAlign && paddingSize != 0) {
@@ -69,17 +84,62 @@ public class ZipAlign {
                 outStream.write(padding);
             }
 
-            // pass all the data
-            passBytes(inBuffer, outStream, compressedSize);
+            // if there isn't any data descriptor we can just pass the data right away
+            if (!hasDataDescriptor) {
+                passBytes(inBuffer, outStream, compressedSize);
+
+                outStream.flush();
+                header = inBuffer.getInt();
+
+                continue;
+            }
+
+            // we have a data descriptor
+
+            // fixme: pkware's spec 4.3.9.3 - although crazy rare, it is possible for data descriptors to not have
+            //        a header before it. It's very tricky to implement it so I prefer to do it later.
+
+            byte[] buffer = new byte[4];
+            ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+
+            // loop until we stumble upon 0x08074b50
+            byte cur;
+            do {
+                cur = inBuffer.get();
+                outStream.write(cur);
+
+                if (byteBuffer.position() == buffer.length - 1) {
+                    // the buffer is full, so we shift all of it to the left
+                    buffer[0] = buffer[1];
+                    buffer[1] = buffer[2];
+                    buffer[2] = buffer[3];
+                    // then put our byte on the last index
+                    byteBuffer.put(buffer.length - 1, cur);
+                } else {
+                    byteBuffer.put(cur);
+                }
+            } while (byteBuffer.getInt(0) != 0x08074b50);
+
+            System.out.println("found data descriptor at " + outStream.bytesWritten());
+
+            // we skip all the data descriptor lol, we don't need it
+            passBytes(inBuffer, outStream, 12);
 
             outStream.flush();
+
+            // todo: zip64
+            // if (zip64) passBytes(inBuffer, outStream, 20);
+
+            // next should be a new header
+            header = inBuffer.getInt();
         }
 
         int centralDirectoryPosition = outStream.bytesWritten();
         int fileOffsetIndex = 0;
 
         // we're at the central directory
-        do {
+        while (header == 0x02014b50) {
             outStream.writeInt(0x02014b50);
             int fileOffset = fileOffsets.get(fileOffsetIndex);
 
@@ -100,14 +160,22 @@ public class ZipAlign {
             inBuffer.getInt();
             outStream.writeInt(fileOffset);
 
-            passBytes(inBuffer, outStream, fileNameLen);
+            byte[] fileName = new byte[fileNameLen];
+
+            inBuffer.get(fileName);
+            outStream.write(fileName);
+
             passBytes(inBuffer, outStream, extraFieldLen);
             passBytes(inBuffer, outStream, fileCommentLen);
 
             outStream.flush();
             fileOffsetIndex++;
 
-        } while (inBuffer.getInt() == 0x02014b50);
+            header = inBuffer.getInt();
+        }
+
+        if (header != 0x06054b50)
+            throw new IOException("No end of central directory record header, there is something wrong");
 
         // end of central directory record
         outStream.writeInt(0x06054b50);
