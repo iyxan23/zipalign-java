ed656a85f60c957427ea1f8277241acc372e1533
Iyxan23 <nurihsanalghifari@gmail.com>
2022-05-16 20:41:56 +0700

    fix: missing java.io.IOException import

--- src/main/java/com/iyxan23/zipalignjava/Main.java
+++ src/main/java/com/iyxan23/zipalignjava/Main.java
@@ -3,6 +3,7 @@ package com.iyxan23.zipalignjava;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
