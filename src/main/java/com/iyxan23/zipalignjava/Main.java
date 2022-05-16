40301f418c1ac1a10cbb10c52af2a69fdd3aa888
Pranav <purwarpranav80@gmail.com>
2022-05-16 17:57:29 +0530

    Check if provided input path is a directory

--- src/main/java/com/iyxan23/zipalignjava/Main.java
+++ src/main/java/com/iyxan23/zipalignjava/Main.java
@@ -1,6 +1,8 @@
 package com.iyxan23.zipalignjava;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -19,8 +21,13 @@ public class Main {
             System.exit(1);
         }
 
+        if (inZip.isDirectory()) {
+            System.err.println("Input path must be of a file: " + inZip.getPath());
+            System.exit(1);
+        }
+
         if (!outZip.createNewFile()) {
-            System.err.println("File already exists: " + outZip.getPath());
+            System.err.println("Output file already exists: " + outZip.getPath());
             System.exit(1);
         }
 
