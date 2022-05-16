5ed443d102263a09ae859f5bfdaf0a011170b3e3
Iyxan23 <nurihsanalghifari@gmail.com>
2022-05-16 12:25:49 +0700

    feat: create a main class for testing

---
+++ src/main/java/com/iyxan23/zipalignjava/Main.java
@@ -0,0 +1,36 @@
+package com.iyxan23.zipalignjava;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println(
+                "Usage:\n\t<exec> <input zip> <output zip>\n\nExample:\n\tjava -jar zipalign.jar input.zip output.zip"
+            );
+            System.exit(1);
+        }
+
+        File inZip = new File(args[0]);
+        File outZip = new File(args[1]);
+
+        if (!inZip.exists()) {
+            System.err.println("Input file doesn't exist: " + inZip.getPath());
+        }
+
+        if (!outZip.createNewFile()) {
+            System.err.println("File already exists: " + outZip.getPath());
+            System.exit(1);
+        }
+
+        try (FileInputStream in = new FileInputStream(inZip)) {
+            try (FileOutputStream out = new FileOutputStream(outZip)) {
+                System.out.println("Aligning zip");
+                ZipAlign.alignZip(in, out);
+                System.out.println("Zip successfully aligned");
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + inZip);
+        }
+    }
+}
